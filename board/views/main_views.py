718ef1f0fb17d6fd900cbb61240282ae1c4de959
juhoi.kim <juhoi.kim@gmail.com>
2020-09-10 00:10:50 +0900

    ~ 2-05 조회와 템플릿

--- board/views/main_views.py
+++ board/views/main_views.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
 @bp.route('/')
 def index():
-    return 'Board index'
+    return redirect(url_for('question._list'))
 
 
 @bp.route('/hello')
