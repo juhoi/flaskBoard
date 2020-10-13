ea096b58604ae2c2dfcbeccec389052606a29ae4
juhoi.kim <juhoi.kim@gmail.com>
2020-10-14 00:27:20 +0900

    ~ 3-09 글쓴이 표시

--- board/views/auth_views.py
+++ board/views/auth_views.py
@@ -6,6 +6,8 @@ from board import db
 from board.forms import UserCreateForm, UserLoginForm
 from board.models import User
 
+import functools
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
@@ -57,3 +59,12 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = User.query.get(user_id)
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
